737b29c643026415c17d6816ce47dd77fd0ca5e5
Juan Carballo <emanuelcarballo2004@gmail.com>
2026-03-31 21:05:21 -0300

    Estructura del Proyecto

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// =============================================================================
+// PONTO DE ENTRADA — todo app Flutter começa no main()
+// =============================================================================
+
+
+void main() {
+  //runApp(const AppHabitos());
+}
+
+
